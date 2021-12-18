99f315323743a984f3a13be0c7397dc436cc5419
Pignataro67 <Pignataro67@gmail.com>
2021-12-18 05:12:32 -0800

    adding JS

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-function countLetters() {
-  var name = "Patrick";
-  let letters = [];
-  if (letter = a) {
-    return letters.name.push()
-  }
-}
+const codes = document.querySelectorAll('.code')
 
-countLetters();
+codes[0].focus()
+
+codes.forEach((code, idx) => {
+    code.addEventListener('keydown', (e) => {
+        if(e.key >= 0 && e.key <=9) {
+            codes[idx].value = ''
+            setTimeout(() => codes[idx + 1].focus(), 10)
+        } else if(e.key === 'Backspace') {
+            setTimeout(() => codes[idx - 1].focus(), 10)
+        }
+    })
+})
+
+
+// function countLetters() {
+//   var name = "Patrick";
+//   let letters = [];
+//   if (letter = a) {
+//     return letters.name.push()
+//   }
+// }
+
+// countLetters();
